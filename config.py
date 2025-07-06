9a90b01a90cfa7deb3d9817d011cff496fdd23e0
IllyaKol777 <133062066+IllyaKol777@users.noreply.github.com>
2025-07-06 23:40:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from os import getenv
 
 BOT_TOKEN = getenv("BOT_TOKEN", "8127039608:AAG1kI8fKtrP0n8ARRuUVzAbVMPFXrz74AQ")
-DATABASE_URL = "postgresql+asyncpg://dava_bayer:kIa41WLPqldniK2zKCwTbKP7sG4ud3KP@dpg-d1l431er433s73d86e70-a/dava_bayer"
+DATABASE_URL = "postgresql+asyncpg://dava_bayer:oRHlidY8IDrDAfx0MTaYgFqfa4QCCZdM@dpg-d1ldt5h5pdvs73bsf0pg-a/dava_bayer_87zd"
+
